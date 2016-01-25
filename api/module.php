5440e36a32817c6e4dab9145e0d886901bfabbf5
Alex Flores <eflores@mdsol.com>
2016-01-24 22:49:56 -0500

    adds autossh config file persitence
     - read from existing config file
     - write to file
     - include ability to have custom port

--- api/module.php
+++ api/module.php
@@ -24,9 +24,62 @@ class autossh extends Module
       case 'disableAutossh':
       $this->disableAutossh();
       break;
+
+      case 'readConf':
+      $this->readConf();
+      break;
+
+      case 'writeConf':
+      $this->writeConf();
+      break;
     }
   }
 
+// Configuration
+  private function readConf()
+  {
+    $contents = file("/etc/config/autossh.test");
+    $args = preg_split("/\s|\t|:|@|'/", $contents[1]);
+
+    return $this->response = $this->parseArguments(array_filter($args));
+  }
+
+  private function writeConf()
+  {
+    $args = $this->request->data;
+    $location = "/etc/config/autossh.test";
+    $config = $this->buildCommand($args);
+    $command = "sed \"2s|.*|$config|\" /rom/etc/config/autossh > $location";
+    exec($command);
+  }
+
+  private function createSshKey()
+  {
+    $path = "/root/.ssh/id_rsa.autossh";
+    exec("ssh-keygen -f $path -t rsa -N ''");
+  }
+
+  private function buildCommand($args)
+  {
+    return "option ssh '-i /root/.ssh/id_rsa.autossh -N -T -R 0.0.0.0:$args->rport:localhost:$args->lport $args->user@$args->host -p $args->port'";
+  }
+
+  // $args {1: "option", 2: "ssh", 4: "-i", 5: "/etc/dropbear/id_rsa", 6: "-N", 7: "-T", 8: "-R", 9: "2222", 10: "localhost", 11: "22", 12: "user", 13: "host"}
+  private function parseArguments($args)
+  {
+    return array(
+      "host" => $args[13],
+      "keyFile" => $args[5],
+      "rport" => $args[9],
+      "lport" => $args[11],
+      "user" => $args[12],
+      "port" => (!$args[15]) ? "22" : $args[15],
+      "success" => true
+    );
+  }
+
+
+  // Management
 
   private function status()
   {
@@ -36,7 +89,6 @@ class autossh extends Module
       "isEnabled" => $this->isEnabled()
     );
   }
-
 
   private function isRunning()
   {
--- js/module.js
+++ js/module.js
@@ -3,10 +3,7 @@ registerController('autosshMainCtrl', ['$api', '$scope', function($api, $scope) 
   $scope.isRunning = false
   $scope.isEnabled = false
   $scope.getStatus = function () {
-    $api.request({
-      module: 'autossh',
-      action: 'status'
-    }, function(response) {
+    apiCaller("status", function(response) {
       if (response.success) {
         $scope.isRunning = response.isRunning
         $scope.isEnabled = response.isEnabled
@@ -14,32 +11,69 @@ registerController('autosshMainCtrl', ['$api', '$scope', function($api, $scope) 
       console.log(response)
     })
   }
-
   $scope.getStatus()
 
   $scope.startAutossh = function () {
-    apiCaller("startAutossh")
+    apiCaller("startAutossh", handler)
   }
 
   $scope.stopAutossh = function () {
-    apiCaller("stopAutossh")
+    apiCaller("stopAutossh", handler)
   }
 
   $scope.enableAutossh = function () {
-    apiCaller("enableAutossh")
+    apiCaller("enableAutossh", handler)
   }
 
   $scope.disableAutossh = function () {
-    apiCaller("disableAutossh")
+    apiCaller("disableAutossh", handler)
   }
+
 
   function handler (response) {
     console.log(response)
     $scope.getStatus()
   }
 
-  function apiCaller (action) {
-    $api.request({ module: 'autossh', action: action }, handler)
+  function apiCaller (action, cb) {
+    $api.request({ module: 'autossh', action: action }, cb)
+  }
+}])
+
+
+registerController('autosshConfCtrl', ['$api', '$scope', function($api, $scope) {
+
+  $scope.formData = {}
+
+  $scope.readConf = function () {
+    apiCaller('readConf', function(response) {
+      if (response.success) {
+        $scope.formData.user  = response.user
+        $scope.formData.host  = response.host
+        $scope.formData.port  = response.port
+        $scope.formData.rport = response.rport
+        $scope.formData.lport = response.lport
+      }
+      console.log(response)
+    })
+  }
+  $scope.readConf()
+
+  $scope.writeConf = function () {
+    $api.request({
+      module: "autossh",
+      action: "writeConf",
+      data: $scope.formData
+    }, handler)
+  }
+
+
+  function handler (response) {
+    console.log(response)
+  }
+
+  function apiCaller (action, cb) {
+    $api.request({ module: 'autossh', action: action }, cb)
   }
 
 }])
